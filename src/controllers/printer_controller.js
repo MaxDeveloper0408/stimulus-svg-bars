bdc37766e169313b181679b4936bc3f39307abe1
Smart Max <maxdevsmart0408@gmail.com>
2021-01-23 00:50:58 +0300

    init

--- src/controllers/printer_controller.js
+++ src/controllers/printer_controller.js
@@ -2,8 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 	static  values = { 
-		printer: Array,
-		url: String
+		printer_sel: Object
 	}
 	static targets = [  "bardraw",
 	"production", 
@@ -15,14 +14,18 @@ export default class extends Controller {
 	"use_svg",
 	"end_of_life_svg" 
 	]
-
-	connect() {
-		fetch(this.urlValue).then(/* … */)
-		console.log("Stimulus works well");
-	}
-	draw() {
+	showbars() {
+		let printerSel = null;
+		this.printer_selValue.impacts.climate_change;
+		
 		const element = this.bardrawTarget;
 
+		if(element.value == "climate_change") {
+			printerSel = this.printer_selValue.impacts.climate_change;
+		}else if(element.value == "energy_use") {
+			printerSel = this.printer_selValue.impacts.energy_use;
+		}
+		const scale = 5;
 		const production = this.productionTarget;
 		const distribution = this.distributionTarget;
 		const use = this.useTarget;
@@ -33,17 +36,40 @@ export default class extends Controller {
 		const use_svg = this.use_svgTarget;
 		const end_of_life_svg = this.end_of_life_svgTarget;
 
-		production.height.baseVal.newValueSpecifiedUnits(1, 20);
-		production_svg.height.baseVal.newValueSpecifiedUnits(1, 20);
+		production.height.baseVal.newValueSpecifiedUnits(1, printerSel.production*scale);
+		production_svg.height.baseVal.newValueSpecifiedUnits(1, printerSel.production*scale);
 
-		distribution.height.baseVal.newValueSpecifiedUnits(1, 20);
-		distribution_svg.height.baseVal.newValueSpecifiedUnits(1, 20);
+		distribution.height.baseVal.newValueSpecifiedUnits(1, printerSel.distribution*scale);
+		distribution_svg.height.baseVal.newValueSpecifiedUnits(1, printerSel.distribution*scale);
 
-		use.height.baseVal.newValueSpecifiedUnits(1, 20);
-		use_svg.height.baseVal.newValueSpecifiedUnits(1, 20);
+		use.height.baseVal.newValueSpecifiedUnits(1, printerSel.use*scale);
+		use_svg.height.baseVal.newValueSpecifiedUnits(1, printerSel.use*scale);
 
-		end_of_life.height.baseVal.newValueSpecifiedUnits(1, 20);
-		end_of_life_svg.height.baseVal.newValueSpecifiedUnits(1, 20);
-		
+		end_of_life.height.baseVal.newValueSpecifiedUnits(1, printerSel.end_of_life*scale);
+		end_of_life_svg.height.baseVal.newValueSpecifiedUnits(1, printerSel.end_of_life*scale);
+	}
+	connect() {
+		console.log("Stimulus works well");
+		this.printer_selValue = {
+			name: "Inkjet Printer",
+			impacts: {
+				climate_change: {
+					production: 45,
+					distribution: 23,
+					use: 12,
+					end_of_life: 5
+				},
+				energy_use: {
+					production: 32,
+					distribution: 5,
+					use: 28,
+					end_of_life: 1
+				}
+			}
+		}
+		this.showbars();
+	}
+	draw() {
+		this.showbars();
 	}
 }
